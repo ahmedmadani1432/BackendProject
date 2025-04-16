5e9490c50ddff0b03b3f601aee1d471e29030463
ahmedmadani1432 <samadani1432@gmail.com>
2025-04-16 17:55:05 +0530

    Initial commit

--- controllers/admin_cntrls.js
+++ controllers/admin_cntrls.js
@@ -1,11 +1,210 @@
 
 
 
+// const admin_Mdl = require("../models/admin_model");
+// const tutor_Mdl = require("../models/tutor_model");
+// const center_Mdl = require("../models/center_model");
+// const attend_Mdl = require("../models/attendance_model");
+// const student_Mdl = require("../models/student_model");
+
+// const {
+//     responseGenerator,
+//     hashPassword,
+//     comparePassword,
+//     generateTokens
+// } = require("../utils/util");
+
+// const register = async (req, res) => {
+//     try {
+//         const admin = new admin_Mdl(req.body);
+//         await admin.save();
+//         res.status(200).json(responseGenerator(true, "Admin registered successfully!", admin));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const login = async (req, res) => {
+//     try {
+//         const { userEmail, userPassword } = req.body;
+//         const admin = await admin_Mdl.findOne({ userEmail: userEmail }).lean();
+//         if (admin && comparePassword(userPassword, admin.userPassword)) {
+//             const tokens = generateTokens({
+//                 email,
+//                 name: admin.fullName,
+//                 id: admin._id
+//             });
+//             return res.status(200).json({
+//                 success: true,
+//                 message: "Admin logged in successfully!",
+//                 data: {
+//                     name: admin.fullName,
+//                     id: admin._id,
+//                     tokens
+//                 }
+//             });
+//         }
+//         res.status(401).json({ success: false, message: "Invalid email or password" });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const dashboard = async (req, res) => {
+//     try {
+//         const centers = await center_Mdl.find();
+//         const tutors = await tutor_Mdl.find();
+//         res.status(200).json({ centers, tutors });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const addCenter = async (req, res) => {
+//     try {
+//         const center = new center_Mdl(req.body);
+//         await center.save();
+//         res.status(200).json(responseGenerator(true, "Center created successfully!", center));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const getUsersList = async (req, res) => {
+//     try {
+//         const tutors = await tutor_Mdl.find().populate("center", "center location contact -_id");
+//         res.status(200).json(responseGenerator(true, "Tutors list fetched!", tutors));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const getUsersByCenterId = async (req, res) => {
+//     try {
+//         const tutors = await tutor_Mdl.find({ center: req.params.id }).populate("center", "center location contact");
+//         const msg = tutors.length ? "Tutors in center fetched" : "No tutors found for this center";
+//         res.status(200).json(responseGenerator(true, msg, tutors));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const getAllStudentsAtCenter = async (req, res) => {
+//     try {
+//         const students = await student_Mdl.find({ center: req.params.id }).populate("center", "center location contact");
+//         const msg = students.length ? "Students at center fetched" : "No students at this center";
+//         res.status(200).json(responseGenerator(true, msg, students));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const getAllStudentsByTutor = async (req, res) => {
+//     try {
+//         const students = await student_Mdl.find({ tutor: req.params.id }).populate("tutor", "fullName email");
+//         const msg = students.length ? "Students under tutor fetched" : "No students found under this tutor";
+//         res.status(200).json(responseGenerator(true, msg, students));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const allStudents = async (req, res) => {
+//     try {
+//         const students = await student_Mdl.find();
+//         const msg = students.length ? "All students fetched" : "No students yet";
+//         res.status(200).json(responseGenerator(true, msg, students));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const attendanceList = async (req, res) => {
+//     try {
+//         const attendance = await attend_Mdl.find().populate("userId", "fullName -_id");
+//         res.status(200).json(responseGenerator(true, "All attendance records fetched", attendance));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const attendanceHistory = async (req, res) => {
+//     try {
+//         const history = await attend_Mdl.find({ userEmail: req.body.userEmail }).sort({ attendanceDate: 1 });
+//         const msg = history.length ? "Attendance history found" : "No history for this tutor";
+//         res.status(200).json(responseGenerator(true, msg, history));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const deleteUser = async (req, res) => {
+//     try {
+//         const { userEmail } = req.body;
+//         const tutor = await tutor_Mdl.findOne({ userEmail });
+//         if (!tutor) throw new Error("Tutor not found");
+//         await tutor_Mdl.deleteOne({ _id: tutor._id });
+//         res.status(200).json(responseGenerator(true, "Tutor deleted successfully"));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(404).json(responseGenerator(false, err.message));
+//     }
+// };
+
+// const deleteAttendance = async (req, res) => {
+//     try {
+//         const { userEmail } = req.body;
+//         await attend_Mdl.findOneAndDelete({ userEmail });
+//         res.status(200).json(responseGenerator(true, "Attendance deleted"));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// const getAllCenters = async (req, res) => {
+//     try {
+//         const centers = await center_Mdl.find();
+//         res.status(200).json(responseGenerator(true, "All centers fetched", centers));
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(responseGenerator(false));
+//     }
+// };
+
+// module.exports = {
+//     register,
+//     login,
+//     dashboard,
+//     addCenter,
+//     getUsersList,
+//     getUsersByCenterId,
+//     getAllStudentsAtCenter,
+//     getAllStudentsByTutor,
+//     allStudents,
+//     attendanceList,
+//     attendanceHistory,
+//     deleteUser,
+//     deleteAttendance,
+//     getAllCenters
+// };
 const admin_Mdl = require("../models/admin_model");
 const tutor_Mdl = require("../models/tutor_model");
 const center_Mdl = require("../models/center_model");
 const attend_Mdl = require("../models/attendance_model");
 const student_Mdl = require("../models/student_model");
+const studentAttend_Mdl = require("../models/studentAttendance_model");
 
 const {
     responseGenerator,
@@ -31,7 +230,7 @@ const login = async (req, res) => {
         const admin = await admin_Mdl.findOne({ userEmail: userEmail }).lean();
         if (admin && comparePassword(userPassword, admin.userPassword)) {
             const tokens = generateTokens({
-                email,
+                email: admin.userEmail,
                 name: admin.fullName,
                 id: admin._id
             });
@@ -183,6 +382,53 @@ const getAllCenters = async (req, res) => {
     }
 };
 
+// ==== Student Attendance Controllers ====
+
+const getAllStudentAttendance = async (req, res) => {
+    try {
+        const records = await studentAttend_Mdl.find()
+            .populate("studentId", "fullName studentRoll center")
+            .sort({ attendanceDate: -1 });
+
+        const msg = records.length ? "Student attendance records fetched" : "No records found";
+        res.status(200).json(responseGenerator(true, msg, records));
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(responseGenerator(false));
+    }
+};
+
+const getStudentAttendanceById = async (req, res) => {
+    try {
+        const { studentId } = req.params;
+        const records = await studentAttend_Mdl.find({ studentId }).sort({ attendanceDate: -1 });
+
+        const msg = records.length ? "Attendance history fetched" : "No records found for this student";
+        res.status(200).json(responseGenerator(true, msg, records));
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(responseGenerator(false));
+    }
+};
+
+const deleteStudentAttendance = async (req, res) => {
+    try {
+        const { attendanceId } = req.params;
+        const deleted = await studentAttend_Mdl.findByIdAndDelete(attendanceId);
+
+        if (!deleted) {
+            return res.status(404).json(responseGenerator(false, "Attendance record not found"));
+        }
+
+        res.status(200).json(responseGenerator(true, "Attendance record deleted", deleted));
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(responseGenerator(false));
+    }
+};
+
+// ==== Exports ====
+
 module.exports = {
     register,
     login,
@@ -197,5 +443,8 @@ module.exports = {
     attendanceHistory,
     deleteUser,
     deleteAttendance,
-    getAllCenters
+    getAllCenters,
+    getAllStudentAttendance,
+    getStudentAttendanceById,
+    deleteStudentAttendance
 };
--- controllers/tutor_cntrl.js
+++ controllers/tutor_cntrl.js
@@ -1,181 +1,194 @@
 
 
+const moment = require("moment");
+
 const tutor_Mdl = require("../models/tutor_model");
 const center_Mdl = require("../models/center_model");
 const student_Mdl = require("../models/student_model");
 const attend_Mdl = require("../models/attendance_model");
-const studentAttendance_Mdl = require("../models/studentsAttend_Model");
-const moment = require("moment");
-const { responseGenerator, hashPassword, comparePassword, generateTokens } = require("../utils/util");
+const studentAttendance_Mdl = require("../models/studentAttendance_model");
 
+const {
+    responseGenerator,
+    hashPassword,
+    comparePassword,
+    generateTokens
+} = require("../utils/util");
+
+// ==========================
+// Tutor Signup
+// ==========================
 const signup = async (req, res) => {
     try {
         const data = req.body;
+
         const existingUser = await tutor_Mdl.findOne({ email: data.email });
         if (existingUser) {
-            return res.status(400).json(responseGenerator(false, "Email Already exists in the list"));
+            return res.status(400).json(responseGenerator(false, "Email already exists"));
         }
 
-        const centerExists = await center_Mdl.findOne({ _id: data.center });
+        const centerExists = await center_Mdl.findById(data.center);
         if (!centerExists) {
-            return res.status(400).json(responseGenerator(false, "Center doesn't exist. Please contact admin to create center."));
+            return res.status(400).json(responseGenerator(false, "Center does not exist"));
         }
 
-        let id = await tutor_Mdl.countDocuments();
-        id = id + 1;
+        const tutorCount = await tutor_Mdl.countDocuments();
+        data.id = tutorCount + 1;
 
-        // Hash password before saving
         data.password = await hashPassword(data.password);
 
-        const user = new tutor_Mdl({ ...data, id });
-        await user.save();
+        const newTutor = new tutor_Mdl(data);
+        await newTutor.save();
 
-        res.status(201).json(responseGenerator(true, "User Added successfully!", user));
+        res.status(201).json(responseGenerator(true, "Tutor registered successfully", newTutor));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
+
+// Tutor Login
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await tutor_Mdl.findOne({ email }).lean();
 
-        if (!user) {
+        const tutor = await tutor_Mdl.findOne({ email }).lean();
+        if (!tutor) {
             return res.status(401).json(responseGenerator(false, "Invalid email or password"));
         }
 
-        const isPasswordMatches = await comparePassword(password, user.password); // await password comparison
-        if (isPasswordMatches) {
-            const tokens = generateTokens({ email, name: user.name, id: user.id });
-            res.status(200).json({
-                success: true,
-                message: "User logged in successfully!",
-                data: { name: user.name, id: user.id, tokens }
-            });
-        } else {
-            res.status(401).json(responseGenerator(false, "Invalid email or password"));
+        const isMatch = await comparePassword(password, tutor.password);
+        if (!isMatch) {
+            return res.status(401).json(responseGenerator(false, "Invalid email or password"));
         }
+
+        const tokens = generateTokens({
+            email,
+            name: tutor.name,
+            id: tutor.id
+        });
+
+        res.status(200).json(responseGenerator(true, "Tutor logged in successfully", {
+            name: tutor.name,
+            id: tutor.id,
+            tokens
+        }));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
+
+// Mark Tutor Attendance
 const MarkAttendance = async (req, res) => {
     try {
         const { email } = req.body;
         const today = moment().format("YYYY-MM-DD");
         const currentTime = moment().format("HH:mm");
 
-        let attendance = await attend_Mdl.findOne({ email, date: today });
-        if (attendance) {
-            return res.status(200).json(responseGenerator(true, "Already marked today's attendance."));
+        const alreadyMarked = await attend_Mdl.findOne({ email, date: today });
+        if (alreadyMarked) {
+            return res.status(200).json(responseGenerator(true, "Attendance already marked for today"));
         }
 
-        const newAttendance = new attend_Mdl({ email, date: today, time: currentTime });
-        await newAttendance.save();
+        const attendance = new attend_Mdl({ email, date: today, time: currentTime });
+        await attendance.save();
 
-        res.status(201).json(responseGenerator(true, "Attendance marked successfully."));
+        res.status(201).json(responseGenerator(true, "Attendance marked successfully"));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
+
+// Mark Student Attendance
 const StudentAttendance = async (req, res) => {
     try {
         const { roll_no } = req.body;
         const today = moment().format("YYYY-MM-DD");
         const currentTime = moment().format("HH:mm");
 
-        let attendance = await studentAttendance_Mdl.findOne({ roll_no, date: today });
-        if (attendance) {
-            return res.status(400).json(responseGenerator(true, "Today's Attendance already marked."));
+        const alreadyMarked = await studentAttendance_Mdl.findOne({ roll_no, date: today });
+        if (alreadyMarked) {
+            return res.status(400).json(responseGenerator(true, "Attendance already marked for this student today"));
         }
 
-        const newAttendance = new studentAttendance_Mdl({ roll_no, date: today, time: currentTime });
-        await newAttendance.save();
+        const attendance = new studentAttendance_Mdl({ roll_no, date: today, time: currentTime });
+        await attendance.save();
 
-        res.status(201).json(responseGenerator(true, "Attendance marked successfully.", newAttendance));
+        res.status(201).json(responseGenerator(true, "Student attendance marked successfully", attendance));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
+
+// Create Student
 const createStudent = async (req, res) => {
     try {
         const data = req.body;
-        const student = await student_Mdl.findOne({ name: data.name, fname: data.fname });
-        if (student) {
-            return res.status(400).json(responseGenerator(false, "Student already exists."));
+
+        const exists = await student_Mdl.findOne({ name: data.name, fname: data.fname });
+        if (exists) {
+            return res.status(400).json(responseGenerator(false, "Student already exists"));
         }
 
-        const count = await student_Mdl.countDocuments();
-        const rollno = count + 1;
+        const studentCount = await student_Mdl.countDocuments();
+        const rollno = studentCount + 1;
 
         const newStudent = new student_Mdl({ ...data, rollno });
         await newStudent.save();
 
-        res.status(201).json(responseGenerator(true, "Student added successfully.", newStudent));
+        res.status(201).json(responseGenerator(true, "Student added successfully", newStudent));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
-// const attendanceOfAllStudents = async (req, res) => {
-//     try {
-//         const studentAttendanceList = await studentAttendance_Mdl.find();
-//         if (studentAttendanceList.length === 0) {
-//             return res.status(200).json(responseGenerator(true, "No attendance recorded yet."));
-//         }
-
-//         res.status(200).json(responseGenerator(true, "Attendance list of all students.", studentAttendanceList));
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(responseGenerator(false, "Internal Server Error"));
-//     }
-// };
-
+// All Students' Attendance
 const attendanceOfAllStudents = async (req, res) => {
     try {
-        const studentAttendanceList = await studentAttendance_Mdl.find();
-        if (studentAttendanceList.length === 0) {
-            return res.status(200).json(responseGenerator(true, "No attendance recorded yet."));
-        }
-        res.status(200).json(responseGenerator(true, "Attendance list of all students.", studentAttendanceList));
+        const records = await studentAttendance_Mdl.find();
+
+        const msg = records.length
+            ? "Attendance list of all students"
+            : "No attendance recorded yet";
+
+        res.status(200).json(responseGenerator(true, msg, records));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
- 
+
+// Attendance History of a Student
 const attendanceHistoryOfStudent = async (req, res) => {
     try {
         const { roll_no } = req.body;
-        const history = await studentAttendance_Mdl.find({ roll_no });
 
+        const history = await studentAttendance_Mdl.find({ roll_no });
         if (history.length === 0) {
-            return res.status(400).json(responseGenerator(true, "No attendance history for this student."));
+            return res.status(404).json(responseGenerator(true, "No attendance history for this student"));
         }
 
-        res.status(200).json(responseGenerator(true, "Attendance history for this student.", history));
+        res.status(200).json(responseGenerator(true, "Student attendance history", history));
     } catch (err) {
-        console.log(err);
-        res.status(500).json(responseGenerator(false, "Internal Server Error"));
+        console.error(err);
+        res.status(500).json(responseGenerator(false, "Internal server error"));
     }
 };
 
 module.exports = {
-        login,
-        signup,
-        MarkAttendance,
-        createStudent,
-        StudentAttendance,
-        attendanceOfAllStudents, 
-        attendanceHistoryOfStudent 
-    
+    signup,
+    login,
+    MarkAttendance,
+    StudentAttendance,
+    createStudent,
+    attendanceOfAllStudents,
+    attendanceHistoryOfStudent
 };
